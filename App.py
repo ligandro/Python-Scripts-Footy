9c23c77ed9d68db2f8ad6b5c109c43d3d219564e
Ligandro Singh Yumnam <97714265+ligandro@users.noreply.github.com>
2023-03-22 20:34:10 +0530

    Update App.py

--- App.py
+++ App.py
@@ -3,25 +3,7 @@ from pizza import show_pizza
 from radar import show_radar
 
 page = st.selectbox("Pizza Plots or Comparision Radars", ("Pizza", "Radar"))
-page_bg_img = f"""
-<style>
-[data-testid="stAppViewContainer"] > .main {{
-background-image: url("https://images.unsplash.com/photo-1475650522725-015d35677789?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80");
-background-size: 100%;
-background-position: top left;
-background-repeat: no-repeat;
-background-attachment: local;
-}}
-[data-testid="stHeader"] {{
-background: rgba(0,0,0,0);
-}}
-[data-testid="stToolbar"] {{
-right: 2rem;
-}}
-</style>
-"""
 
-st.markdown(page_bg_img, unsafe_allow_html=True)
 if page == "Pizza":
     show_pizza()
 else:
