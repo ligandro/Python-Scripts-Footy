6576162f1f09b24b06dc36e7c62721fdec398414
Ligandro Singh Yumnam <97714265+ligandro@users.noreply.github.com>
2023-03-22 09:19:52 +0530

    Create App.py

---
+++ App.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from pizza import show_pizza
+from radar import show_radar
+
+page = st.sidebar.selectbox("Radar Or Predict", ("Predict", "Explore"))
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("https://images.unsplash.com/photo-1475650522725-015d35677789?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80");
+background-size: 100%;
+background-position: top left;
+background-repeat: no-repeat;
+background-attachment: local;
+}}
+[data-testid="stHeader"] {{
+background: rgba(0,0,0,0);
+}}
+[data-testid="stToolbar"] {{
+right: 2rem;
+}}
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+if page == "Predict":
+    show_predict_page()
+else:
+    show_explore_page()
