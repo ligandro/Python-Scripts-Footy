31469e0f11c8f207f9217a83f7da59909d505872
Ligandro Singh Yumnam <97714265+ligandro@users.noreply.github.com>
2023-08-02 23:35:05 +0530

    s

--- report.py
+++ report.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 import urllib
 import json
 import yaml
-
+import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
