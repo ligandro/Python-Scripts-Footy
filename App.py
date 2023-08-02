9ef6cce3cb739ec30c2d4591d69c873b622dc3b4
Ligandro Singh Yumnam <97714265+ligandro@users.noreply.github.com>
2023-08-02 22:39:06 +0530

    s

--- App.py
+++ App.py
@@ -1,10 +1,15 @@
 import streamlit as st
 from pizza import show_pizza
 from radar import show_radar
+from report import show_report
 
-page = st.selectbox("Pizza Plots or Comparision Radars", ("Pizza", "Radar"))
+status1 = [ "Player Match Report", "Pizza Plots" ,"Comparision Radars"]
+           
+page = st.sidebar.radio("Options",(status1))
 
 if page == "Pizza":
     show_pizza()
+elif page == "Player Match Report":
+    show_report()
 else:
     show_radar()
