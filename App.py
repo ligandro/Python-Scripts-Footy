43e963401d3f99f0acd8adce5fc93ce021bcdf16
Ligandro Singh Yumnam <97714265+ligandro@users.noreply.github.com>
2023-08-02 23:27:02 +0530

    s

--- App.py
+++ App.py
@@ -3,13 +3,14 @@ from pizza import show_pizza
 from radar import show_radar
 #from report import show_report
 
-status1 = [  "Pizza Plots" ,"Comparision Radars","Player Match Report"]
+status1 = [  "Pizza Plots" ,"Comparision Radars"]
+           #,"Player Match Report"]
            
 page = st.sidebar.radio("Options",(status1))
 
 if page == "Pizza Plots":
     show_pizza()
-elif page == "Player Match Report":
+#elif page == "Player Match Report":
     show_report()
 else:
     show_radar()
