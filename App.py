ad8762e3814f74afd3cd5655d438149773da44e3
Ligandro Singh Yumnam <97714265+ligandro@users.noreply.github.com>
2023-08-03 11:21:45 +0530

    s

--- App.py
+++ App.py
@@ -1,15 +1,16 @@
 import streamlit as st
 from pizza import show_pizza
 from radar import show_radar
-from report import show_report
+#from report import show_report
 
-status1 = [  "Pizza Plots" ,"Comparision Radars","Player Match Report"]
+status1 = [  "Pizza Plots" ,"Comparision Radars"]
+           #,"Player Match Report"]
            
 page = st.sidebar.radio("Options",(status1))
 
 if page == "Pizza Plots":
     show_pizza()
-elif page == "Player Match Report":
-    show_report()
+#elif page == "Player Match Report":
+    #show_report()
 else:
     show_radar()
